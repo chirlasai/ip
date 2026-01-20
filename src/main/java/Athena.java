b86755b5248ad1640ff998d061866860233c0a3d
Chirla Sai <chirlasai907@gmail.com>
2026-01-20 17:32:11 +0800

    renamed chatbot and updated skeletal version

---
+++ src/main/java/Athena.java
@@ -0,0 +1,16 @@
+public class Athena {
+    public static void main(String[] args) {
+        String name = "Athena";
+        String line = "________________________________________________________________";
+        // greetings
+        System.out.println(line);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        // good bye
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+
+       
+    }
+}
