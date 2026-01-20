33b5c2d4e7aca3a5ba1d946035904b02a6774c13
Chirla Sai <chirlasai907@gmail.com>
2026-01-20 17:57:29 +0800

    improved skeletal code

--- src/main/java/Athena.java
+++ src/main/java/Athena.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Athena {
     public static void main(String[] args) {
         String name = "Athena";
@@ -7,10 +9,26 @@ public class Athena {
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println(line);
-        // good bye
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        // input reading and response
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine(); // Read user input
+
+            System.out.println(line);
+            
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break; // Exit the loop
+            } else {
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
+        
+        scanner.close();
+    }
 
        
-    }
 }
