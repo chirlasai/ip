b2f25968023a2b46e2d42b31f091c9d518bd8471
Chirla Sai <chirlasai907@gmail.com>
2026-01-20 19:38:09 +0800

    added marking functionality

--- src/main/java/Athena.java
+++ src/main/java/Athena.java
@@ -4,7 +4,7 @@ public class Athena {
     public static void main(String[] args) {
         String name = "Athena";
         String line = "________________________________________________________________";
-        String[] storage = new String[100];
+        Task[] tasks = new Task[100];
         int itemCount = 0;
         // greetings
         System.out.println(line);
@@ -24,20 +24,26 @@ public class Athena {
                 System.out.println(line);
                 break; // Exit the loop
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < itemCount; i++) {
-                    System.out.println((i + 1) + ". " + storage[i]);
+                    System.out.println((i + 1) + ". " + tasks[i].toString());
                 }
-                System.out.println(line);
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                tasks[index].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n  " + tasks[index]);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                tasks[index].unmark();
+                System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[index]);
             } else {
-                storage[itemCount] = input;
+                tasks[itemCount] = new Task(input);
                 itemCount++;
                 System.out.println("added: " + input);
+            }
                 System.out.println(line);
             }
-        }
         
         scanner.close();
     }
-
-       
 }
